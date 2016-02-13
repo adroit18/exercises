5f9a5a9dd5ba7bfcacf5e79271edb46dc0e1fb42
adroit18 <deepak.uniyal@tothenew.com>
2016-02-13 09:29:32 +0530

    second commit

--- link_sharing/grails-app/conf/BootStrap.groovy
+++ link_sharing/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,10 @@
 class BootStrap {
+def grailsApplication
+    def init = {
 
-    def init = { servletContext ->
+
+        //""${userHome}/.grails/${appName}-config.groovy""
+      println "External file is included"+grailsApplication.config.myname
     }
     def destroy = {
     }
